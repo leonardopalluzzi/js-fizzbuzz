5abfd0a7f4b2ed33f2d77c36923cdb7ac729e242
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-11 15:52:15 +0100

    add comments, exercise complete

--- main.js
+++ main.js
@@ -8,20 +8,28 @@ Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stamp
 // scrivere condizione per i multipli di 5 e stampare buzz
 // scrivere condizione per i multipli di 3 e 5 e stampare fizzbuzz
 
+// variables declaration
 const n = 100;
 let restThree;
 let restFive;
 
+// for loop
  for (let i = 1; i <= n; i++){
+    // rest calc
     restThree = i % 3;
     restFive = i % 5;
+    // rest check
     if (restThree == 0 && restFive == 0){
+        // multiples of three and five
         console.log(`${i} FizzBuzz`);
     } else if (restFive == 0){
+        // multiples of five
         console.log(`${i} Buzz`);  
     } else if (restThree == 0) {
+        // multiples of three
         console.log(`${i} Fizz`);
     } else {
+        // not mutiple
         console.log(i);
     }
  }
