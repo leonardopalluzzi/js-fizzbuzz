735e1cca2f5fa9bf89c4a75c5a02ae69ea08f69b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-11 15:35:32 +0100

    fix human code

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /* Consegna:
 Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
+// inizializzare variabili
 // creare ciclo for per i che va da 1 a 100 (100 incluso)
 // calcolare il resto calcolare il resto
 // scrivere condiozione per i multipli di 3 e stampare fizz
 // scrivere condizione per il multipli di 5 e stampare buzz
 // scrivere condizione per i multipl;i di 3 e 5 e stampare fizzbuzz
+
